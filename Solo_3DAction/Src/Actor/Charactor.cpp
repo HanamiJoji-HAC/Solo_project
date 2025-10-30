e656e42b06342b579855622da54547d9190e846d
Hanami Joji <g023g1023@g.neec.ac.jp>
2025-10-31 00:03:59 +0900

    Actorを継承しゲームプレイシーンで利用されるActorの親になるクラスを新規追加

---
+++ Solo_3DAction/Src/Actor/Charactor.cpp
@@ -0,0 +1,86 @@
+#include "Charactor.h"
+#include "GameConfig.h"
+
+Charactor::Charactor(const Status& status) : status_(status) {}
+
+void Charactor::take_damage(Actor& other, int damage) {
+	status_.hp_ = CLAMP(status_.hp_ - damage, 0, INT_MAX);
+}
+
+void Charactor::update_gravity(float delta_time, float gravity) {
+	velocity_.y += (delta_time / cREF) * gravity;
+	transform_.translate(0.0f, velocity_.y, 0.0f);
+    collide_field();
+};
+
+void Charactor::move(float delta_time, float move_speed) {}
+
+void Charactor::boost(float delta_time, float boost_power) {}
+
+void Charactor::collide_actor(Actor& other) {
+    // ｙ座標を除く座標を求める
+    GSvector3 position = transform_.position();
+    position.y = 0.0f;
+    GSvector3 target = other.transform().position();
+    target.y = 0.0f;
+    // 相手との距離
+    float distance = GSvector3::distance(position, target);
+    // 衝突判定球の半径同士を加えた長さを求める
+    float length = collider_.radius + other.collider().radius;
+    // 衝突判定球の重なっている長さを求める
+    float overlap = length - distance;
+    // 重なっている部分の半分の距離だけ離れる移動量を求める
+    GSvector3 v = (position - target).getNormalized() * overlap * 0.5f;
+    transform_.translate(v, GStransform::Space::World);
+    // フィールドとの衝突判定
+    collide_field();
+};
+
+void Charactor::collide_field() {
+    // 壁との衝突判定（球体との判定）
+    GSvector3 center; // 押し戻し後の球体の中心座標
+    if (world_->field()->collide(collider(), &center)) {
+        // y座標は変更しない
+        center.y = transform_.position().y;
+        // 補正後の座標に変更する
+        transform_.position(center);
+    }
+    // 地面との衝突判定（線分との交差判定）
+    GSvector3 position = transform_.position();
+    Line line;
+    line.start = position + collider_.center;
+    line.end = position + GSvector3{ 0.0f, -foot_offset_, 0.0f };
+    GSvector3 intersect; // 地面との交点
+    if (world_->field()->collide(line, &intersect)) {
+        // 交点の位置からy座標のみ補正する
+        position.y = intersect.y;
+        // 座標を変更する
+        transform_.position(position);
+        // 重力を初期化する
+        velocity_.y = 0.0f;
+    }
+};
+
+bool Charactor::check_ground() {
+    //地面との衝突判定(線分との交差判定)
+    GSvector3 position = transform_.position();
+    Line line;
+    line.start = position + collider_.center;
+    line.end = position + GSvector3{ 0.0f,-foot_offset_,0.0f };
+    GSvector3 intersect; //地面との交点
+
+    if (world_->field()->collide(line, &intersect)) {
+        //交点の位置からy座標のみ補正する
+        position.y = intersect.y;
+        ////座標を変更する
+        transform_.position(position);
+        //重力を初期化する
+        velocity_.y = 0.0f;
+        return true;
+    }
+    return false;
+}
+
+void Charactor::generate_attack_collider() {}
+
+void Charactor::generate_bullet_collider() {}
---
+++ Solo_3DAction/Src/Actor/Charactor.h
@@ -0,0 +1,79 @@
+#ifndef CHARACTOR_H_
+#define CHARACTOR_H_
+
+#include "Actor/Actor.h"
+#include "Collision/AttackCollider.h"
+#include "Player/PlayerBullet.h"
+#include "Input.h"
+
+struct Status
+{
+	// ステータス構造体
+	Status(
+		int hp_				 = 50,
+		float energy_		 = 100,
+		float max_energy_	 = 100,
+		int melee_atk_		 = 5,
+		int ranged_atk_		 = 1,
+		float jump_power_	 = 0.20f,
+		float boost_speed_	 = 4.0f,
+		float walk_speed_	 = 5.0f,
+		float move_speed_	 = 5.0f,
+		float gravity_		 = -0.40f
+	) :
+		hp_{ hp_ },
+		energy_{ energy_ },
+		max_energy_{ max_energy_ },
+		melee_atk_{ melee_atk_ },
+		ranged_atk_{ ranged_atk_ },
+		jump_power_{ jump_power_ },
+		boost_speed_{ boost_speed_ },
+		walk_speed_{ walk_speed_ },
+		move_speed_{ move_speed_ },
+		gravity_{ gravity_ }
+	{ }
+	int hp_{ 0 };
+	float energy_{ 0.0f };
+	float max_energy_{ 0.0f };
+	int melee_atk_{ 0 };
+	int ranged_atk_{ 0 };
+	float jump_power_{ 0.0f };
+	float boost_speed_{ 0.0f };
+	float walk_speed_{ 0.0f };
+	float move_speed_{ 0.0f };
+	float gravity_{ 0.0f };
+};
+class Charactor : public Actor {
+public:
+	Charactor(const Status& status);
+
+	virtual ~Charactor() = default;
+public:
+	virtual void take_damage(Actor& other, int damage);
+	virtual void update_gravity(float delta_time, float grav);
+	virtual bool check_ground();
+protected:
+	virtual void collide_actor(Actor& other);
+	virtual void collide_field();
+	virtual void generate_attack_collider();
+	virtual void generate_bullet_collider();
+	// 移動
+	virtual void move(float delta_time, float move_speed);
+	// ブースト
+	virtual void boost(float delta_time, float boost_power);
+protected:
+	// ステータスパラメータ
+	Status status_;
+	// 重力
+	//float gravity_{ -0.98f };
+	// 身長
+	float height_{ 1.5f };
+	// 足元のオフセット
+	float foot_offset_{ 0.05f };
+	// 無敵時間
+	float inbisible_timer_{ 0.0f };
+	// 死亡判定
+	bool dead_{ false };
+	Input& input_ = Input::get_instance();
+};
+#endif
