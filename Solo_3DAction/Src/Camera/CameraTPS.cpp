daddc32d60125ef52d97c520850d9ff756bd57aa
Hanami Joji <g023g1023@g.neec.ac.jp>
2025-10-31 00:14:21 +0900

    カメラを操作によって自由に動かせるように変更

--- Solo_3DAction/Src/Camera/CameraTPS.cpp
+++ Solo_3DAction/Src/Camera/CameraTPS.cpp
@@ -3,11 +3,15 @@
 #include "World/Field.h"
 #include "Math/Line.h"
 #include "imgui/imgui.h"
+#include <algorithm>
 
-// プレーヤーからの相対座標
-GSvector3 PlayerOffset{ 0.0f, 3.0f, -5.0f };
+
+const float MaxSpeed{ 20.0f }; // 移動スピードの最大値
 // カメラの注視点の補正値
-GSvector3 ReferencePointOffset{ 0.0f, 1.0f, 4.0f };
+const GSvector3 ReferencePointOffset{ 0.0f, 1.0f, 0.0f };
+// プレーヤーからの相対座標
+const GSvector3 PlayerOffset{ 0.0f, 3.0f, -5.0f };
+const float Sencitivity{ 2.0f };
 // コンストラクタ
 CameraTPS::CameraTPS(IWorld* world, const GSvector3& position, const GSvector3& at) {
 	// ワールドを設定
@@ -24,39 +28,26 @@ CameraTPS::CameraTPS(IWorld* world, const GSvector3& position, const GSvector3& 
 
 // 更新
 void CameraTPS::update(float delta_time) {
-	ImGui::Begin("camera");
-	ImGui::DragFloat3("camera", ReferencePointOffset);
-	ImGui::End();
-	//GSvector2 camera_input = input_.get_right_stick_input();
-	//velocity_.x = camera_input.x;
-	//velocity_.y = camera_input.y;
-	//GSvector3 velocity{ 0.0f, 0.0f, 0.0f };
-	//velocity += camera_input * delta_time;
-	//transform_.position(velocity);
-	//if (!gsGetKeyState(GKEY_P)) return;
-	// プレーヤーを検索
-	Actor* player = world_->find_actor("Player");
-	if (player == nullptr) return;
-	// 視点の位置を求める（プレーヤーの背後の座標）
-	GSvector3 position = PlayerOffset * player->transform().localToWorldMatrix();
-	// 注視点の位置を求める（プレーヤーの頭部の少し上あたりの座標）
-	GSvector3 at = player->transform().position() + ReferencePointOffset;
-	// フィールドとの衝突判定
-	Line line{ at, position };
-	GSvector3 intersect;
-	if (world_->field()->collide(line, &intersect)) {
-		position = intersect;
-	}
-	// スムースダンプによる滑らかな補間
-	const float SmoothTime{ 3.0f }; // 補間フレーム数
-	const float MaxSpeed{ 20.0f }; // 移動スピードの最大値
-	position = GSvector3::smoothDamp(transform_.position(), position, velocity_,
-		SmoothTime, MaxSpeed, delta_time);
 
-	// 視点の位置を設定
-	transform_.position(position);
-	// 注視点を設定（注視点の方向に向きを変える）
-	transform_.lookAt(at);
+	// プレーヤーを検索
+	Actor* player = get_player();
+
+	if (player == nullptr) return;
+#ifndef DEBUG
+	ImGui::Begin("camera");
+	ImGui::DragFloat3("look_point", ReferencePointOffset);
+	ImGui::DragFloat3("position", PlayerOffset);
+	ImGui::End();
+#endif // !DEBUG
+
+	if (gsGetKeyTrigger(GKEY_I)) {
+		start_debug = true;
+		return;
+	}
+
+	if (start_debug) return;
+
+	tpp_normal(player, delta_time);
 }
 
 // 描画
@@ -73,4 +64,58 @@ void CameraTPS::draw() const {
 		at.x, at.y, at.z, // 注視点の位置
 		0.0f, 1.0f, 0.0f // 視点の上方向
 	);
+}
+
+Actor* CameraTPS::get_player() const {
+	return world_->find_actor("Player");
+}
+
+void CameraTPS::tpp_normal(Actor* other, float delta_time) {
+	input_.get_right_stick_input_angle();
+	GSvector2 input_lstick = input_.get_right_stick_input_value();
+	update_camera_rotation(input_lstick, delta_time, Sencitivity, pitch_, yaw_);
+
+	// 注視点の位置を求める（プレーヤーの頭部の少し上あたりの座標）
+	GSvector3 at = other->transform().position() + ReferencePointOffset;
+	GSvector3 velocity = velocity_;
+	// 視点の位置を求める（プレーヤーの背後の座標）
+	GSvector3 position = at + GSquaternion::euler(pitch_, yaw_, 0) * PlayerOffset;
+	// フィールドとの衝突判定
+	Line line{ at, position };
+	GSvector3 intersect;
+	if (world_->field()->collide(line, &intersect)) {
+		position = intersect;
+	}
+	// スムースダンプによる滑らかな補間
+	position = GSvector3::smoothDamp(transform_.position(), position, velocity,
+		smooth_time_, MaxSpeed, delta_time);
+	// 視点の位置を設定
+	transform_.position(position);
+	// 注視点を設定（注視点の方向に向きを変える）
+	transform_.lookAt(at);
+
+#ifndef DEBUG
+	ImGui::Begin("tpp_normal");
+	ImGui::DragFloat3("camera_position", position);
+	ImGui::DragFloat3("velocity", velocity);
+	ImGui::DragFloat("pitch", &pitch_);
+	ImGui::DragFloat("yaw", &yaw_);
+	ImGui::End();
+#endif // !DEBUG
+}
+
+void CameraTPS::update_camera_rotation(GSvector2 stickInput, float delta_time, float sensitivity,
+	float& pitch, float& yaw)
+{
+	// スティックの入力がなければ何もしない
+	if (stickInput.x == 0 && stickInput.y == 0) return;
+
+	// 感度・時間でスケーリング
+	yaw -= stickInput.x * sensitivity * delta_time;
+	pitch -= stickInput.y * sensitivity * delta_time;
+
+	// pitch角を制限（上向き・下向きの限界）
+	const float pitchMin = -45.0f;
+	const float pitchMax = 59.0f;
+	pitch = std::clamp(pitch, pitchMin, pitchMax);
 }
--- Solo_3DAction/Src/Camera/CameraTPS.h
+++ Solo_3DAction/Src/Camera/CameraTPS.h
@@ -2,6 +2,7 @@
 #define CAMERA_TPS_H_
 #include "Actor/Actor.h"
 #include "Input.h"
+#include <string>
 // 三人称カメラクラス
 class CameraTPS : public Actor {
 public:
@@ -11,8 +12,18 @@ public:
 	virtual void update(float delta_time) override;
 	// 描画
 	virtual void draw() const override;
+public:
+	Actor* get_player() const;
+	void tpp_normal(Actor* other, float delta_time);
+	bool start_debug{ false };
+	void update_camera_rotation(GSvector2 stickInput, float deltaTime, float sensitivity,
+		float& pitch, float& yaw);
 private:
 	Input& input_ = Input::get_instance();
+	float smooth_time_{ 3.0f }; // 補間フレーム数
+	GSvector3 debug_position{};
+	float yaw_{};
+	float pitch_{};
 };
 
 #endif
