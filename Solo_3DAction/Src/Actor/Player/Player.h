45a8e3bcf1848589cc1f44542c3e6dd628474829
Hanami Joji <g023g1023@g.neec.ac.jp>
2025-10-31 03:52:47 +0900

    無敵の追加、新規ステート追加、死亡中フラグ追加、レイヤー用モーション遷移関数追加

--- Solo_3DAction/Src/Actor/Player/Player.h
+++ Solo_3DAction/Src/Actor/Player/Player.h
@@ -9,7 +9,7 @@
 // プレーヤークラス
 class Player : public Charactor {
 public:
-    // プレーヤーの状態
+    // プレーヤーの状態(必ずInputAction、add_state、Motionに追加を行うこと。)
     enum class State {
         Idle,       // 待機中
         Move,		// 移動中
@@ -20,28 +20,22 @@ public:
         Jump,		// ジャンプ中
         AirIdle,    // 空待機中
         AirMove,    // 空中移動中
-        Boost,      //ブースト中
+        Boost,      // ブースト中
+        Fire,       // 射撃中
     };
     // モーション番号
     enum class Motion {
-        MotionIdle = 37,     //アイドル
-        MotionDamage = 35,     //ダメージ
-        MotionAttack = 55,     //攻撃
-        MotionJump = 40,     //ジャンプ
-        MotionWalkForward = 50,     //歩き（前）
-        MotionWalkBack = 51,     //歩き（後）
-        MotionWalkLeft = 53,     //歩き（左）
-        MotionWalkRight = 54,     //歩き（右）
-        MotionBoost = 34,      //飛ぶ
-        MotionDead = 5,
-        //MotionMove = 52,       // 走り
-        //MotionDown = 3,      // ダウン
-        //MotionGetUp = 31,     // 起き上がり
-        //MotionJumpStart = 2,  // ジャンプ開始
-        //MotionLanding = 31,    // 着地中
-        //MotionJumpAttack = 32,// 空中攻撃中
-        //MotionAttack2 = 4,
-        //MotionAttack3 = 32
+        MotionIdle = 37,            // アイドル
+        MotionDamage = 35,          // ダメージ
+        MotionAttack = 2,          // 攻撃
+        MotionJump = 40,            // ジャンプ
+        MotionWalkForward = 50,     // 歩き（前）
+        MotionWalkBack = 51,        // 歩き（後）
+        MotionWalkLeft = 53,        // 歩き（左）
+        MotionWalkRight = 54,       // 歩き（右）
+        MotionBoost = 34,           // 飛ぶ
+        MotionDead = 5,             // 死亡
+        MotionFire = 55,            // 射撃
     };
 public:
     // コンストラクタ
@@ -50,39 +44,48 @@ public:
     virtual void update(float delta_time) override;
     // 描画
     virtual void draw() const override;
+    // GUI描画
     virtual void draw_gui() const override;
     // 衝突リアクション
     virtual void react(Actor& other) override;
 public:
-    //攻撃
+    // 攻撃
     void attack();
-    //ジャンプ
+    // 射撃
+    void fire();
+    // ジャンプ
     void jump();
     void set_jump(float jumpPower);
-    //移動
+    // 移動
     void move(float delta_time,float move_speed) override;
+    // ブースト
     void boost(float delta_time, float boost_power) override;
-    //死亡
-    //void dead();
-    //弾の生成
-    //状態を変更する
+    // 死亡中
+    bool is_dying() override;
+    // ステートを変更する
     void change_state(State state_num);
-    //モーションを変更する
+    // モーションを変更する
     void change_motion(Motion motion, bool loop);
-    //モーションを変更する(番号指定)
+    // モーションを変更する(番号指定)
     void change_motion(int motion, bool loop);
-    //モーションは終了しているか？
+    // モーションを変更する(レイヤー指定)
+    void change_motion(int layer, Motion motion, bool loop);
+    // モーションを変更する(レイヤー、番号指定)
+    void change_motion(int layer, int motion, bool loop);
+    // モーションは終了しているか？
     bool is_motion_end();
-    //変数を取得する
-    float get_gravity();
 public:
+    // 弾判定の生成
     void generate_bullet_collider() override;
+    // 攻撃判定の生成
     void generate_attack_collider() override;
+    // ステートの追加
     void add_state();
+    // ステートタイマの更新
     void update_state_timer(float delta_time);
-    //float get_boost_power();
+    // 現在のステートを文字列に変換
     const char* current_state_to_string(State state);
-    float get_move_speed();
+    // ステータスを参照する
     Status& get_status();
 public:
     //Tips:変数はプライベートにしてゲッターを作る
