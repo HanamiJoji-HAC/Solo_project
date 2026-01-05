0f3e77add0125b21da9aa65927867e9b5fefc6e4
Hanami Joji <g023g1023@g.neec.ac.jp>
2026-01-05 23:44:55 +0900

    ロックオン処理を追加

--- Solo_3DAction/Src/Camera/CameraTPS.h
+++ Solo_3DAction/Src/Camera/CameraTPS.h
@@ -10,10 +10,17 @@
 // 三人称カメラクラス
 class CameraTPS : public Actor {
 public:
+	// 状態
 	enum class State {
 		Null,       // 空ステート
 		Normal,		// 通常ステート
 		LockOn,		// ロックオンステート
+	};
+	// アングル
+	enum class AngleAxis {
+		Yaw,	// ヨー
+		Pitch,	// ピッチ
+		Roll	// ロール
 	};
 	// コンストラクタ
 	CameraTPS(IWorld* world, const GSvector3& position, const GSvector3& at);
@@ -22,40 +29,69 @@ public:
 	// 描画
 	virtual void draw() const override;
 public:
+	void set_camera(Actor* player, float delta_time);
 	// 通常カメラ操作
 	void tpp_normal(Actor* other, float delta_time);
 	// ヨー・ピッチを計算する
-	void update_tpp_normal(GSvector2 stick_input, float delta_time, float sensitivity);
-	// カメラの滑らかな補間
-	GSvector3 smooth_damp(float speed, float smooth_time, GSvector3 position, GSvector3 velocity, float delta_time);
+	void stick_controll_camera(GSvector2 stick_input, float delta_time, float sensitivity);
 	// カメラ内に対象がいるか？
 	bool is_actor_on_screen(Actor* other);
 	// カメラをリセットする
-	void reset_camera(Actor* player);
+	void reset_camera(float delta_time);
 	// ターゲットをリセットする	
-	void reset_actor();
+	void reset_target();
 	// アクターをロックオンする
 	void lock_on_actor(const std::string& tag);
 	// プレーヤーを検索
 	void find_actor(const std::string& player);
+	// ロックオン
+	void update_lockon(float delta_time);
+	GSvector3 get_yaw_pitch() const;
+	// ロックオン時のヨーを設定
+	void set_lock_on_yaw(Actor* player, Actor* target, float delta_time);
+	// ロックオン時のピッチを設定
+	void set_lock_on_pitch(Actor* player, Actor* target, float delta_time);
 public:
+	// ステートの変更
 	void change_state(State state_num);
+	// ステートの追加
 	void add_state();
-	Actor* get_player();
-private:
-	Input& input_ = Input::get_instance();
-	float smooth_time_{ 3.0f }; // 補間フレーム数
-	GSvector3 debug_position{};
-	float yaw_{ 0.0f };
-	float pitch_{ 0.0f };
-	ActorManager actor_manager_;
+	// プレイヤー情報を取得
+	Actor* get_player() const;
+	// ターゲット情報を取得
+	Actor* get_target() const;
+	GSvector3 get_target_vector() const;
 	// ロックオン状態か？
-	bool is_lock_on_{ false };
+	bool is_lock_on() const;
+	// リセット中か？
+	bool is_reset() const;
+	// 感度倍率を取得
+	float get_senc() const;
+private:
+	// 入力クラス
+	Input& input_ = Input::get_instance();
+	// スムースダンプの補間フレーム数
+	float smooth_time_{ 3.0f };
+	// ヨー
+	float yaw_{ 0.0f };
+	// ピッチ
+	float pitch_{ 0.0f };
 	// ターゲットの対象
 	Actor* target_ = nullptr;
 	// プレーヤー
 	Actor* player_;
+	// ターゲットとのベクトル
+	GSvector3 target_to_player_;
+	bool is_reset_{ false };
+	// HACK:
+	float to_target_distance_;
+	GSvector3 actor_position_;
+	GSvector3 norma_forward_;
+	GSvector3 euler_angle_{};
+	GSvector3 desired_rotate_{};
+	GSvector3 test_position_{};
 };
-using CameraState = Camera::State;
+using CameraState = CameraTPS::State;
+using CameraAngleAxis = CameraTPS::AngleAxis;
 #endif
 
